664f95194832d448181346f0fb42f127e855a999
erick <erickfroes@fdcsistemas.com.br>
2017-04-03 17:32:44 -0300

    retirando lixo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => 'admin@laravel.com',
+            'password' => bcrypt('123'),
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => "admin",
+            'display_name' => 'User Administrator',
+            'description' => 'User is allowed to manage and edit other users',
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 1,
+            'role_id' => 1,
+        ]);
     }
 }
